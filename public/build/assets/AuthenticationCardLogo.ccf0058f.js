c67177233ebb73d937c9c578ebc13040237116b6
Mati Sediqi <mati_01@icloud.com>
2022-10-24 00:04:14 +0200

    fix: add build to vcs

---
+++ public/build/assets/AuthenticationCardLogo.ccf0058f.js
@@ -0,0 +1,1 @@
+import{o,c as a,w as t,b as e,g as r,L as s}from"./app.3f58f459.js";import{A as c}from"./ApplicationLogo.7e24db08.js";const m={__name:"AuthenticationCardLogo",setup(n){return(i,p)=>(o(),a(r(s),{href:"/dashboard"},{default:t(()=>[e(c,{style:{"font-size":"30px"}})]),_:1}))}};export{m as _};
